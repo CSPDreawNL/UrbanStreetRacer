9e13944bcded1dcf45fbcf32692ddc85b3fe8a71
Dreaw Riddleton <dreawriddleton@hotmail.com>
2021-10-29 16:21:00 +0200

    Bezier Curve done

--- SimpleStreetRacer/Assets/Scripts/Controller/AIController.cs
+++ SimpleStreetRacer/Assets/Scripts/Controller/AIController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using PathCreation;
 
 namespace USR.Controller
 {
@@ -10,28 +11,16 @@ namespace USR.Controller
         public float m_TurnSpeed;
         public float m_SpeedModifier;
         public bool m_Finished = false;
+        int m_ForwardInput;
 
-        [SerializeField] private Transform[] routes;
-        private int routeToGo;
-        private float tParam;
-        private Vector2 aiPosition;
-        private bool coroutineAllowed;
-
-        private void Start()
-        {
-            routeToGo = 0;
-            tParam = 0f;
-            coroutineAllowed = true;
-        }
+        public PathCreator pathCreator;
+        public EndOfPathInstruction end;
+        float distanceTravelled;
 
         private void Update()
         {
-            if (coroutineAllowed)
-            {
-                StartCoroutine(GoByTheRoute(routeToGo));
-            }
 
-            int m_ForwardInput = 1;
+            m_ForwardInput = 1;
             if (!m_Finished)
             {
                 if (m_ForwardInput == 1)
@@ -42,7 +31,7 @@ namespace USR.Controller
                     }
                     m_Speed = m_Speed + Time.deltaTime * m_SpeedModifier;
                 }
-                else if (m_ForwardInput == -1)
+                else if (m_ForwardInput < 1)
                 {
                     if (m_Speed >= 0)
                     {
@@ -50,10 +39,17 @@ namespace USR.Controller
                     }
                     m_Speed = m_Speed - Time.deltaTime * m_SpeedModifier;
                 }
+                if (pathCreator != null)
+                {
+                    distanceTravelled += m_Speed * Time.deltaTime;
+                    transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, end);
+                    transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, end);
+                }
             }
 
             if (m_Finished)
             {
+                m_ForwardInput = 0;
                 if (m_Speed <= 0)
                 {
                     m_Speed = m_Speed + Time.deltaTime * m_SpeedModifier * 3;
@@ -68,37 +64,5 @@ namespace USR.Controller
                 }
             }
         }
-
-        private IEnumerator GoByTheRoute(int routeNum)
-        {
-            coroutineAllowed = false;
-
-            Vector3 p0 = routes[routeNum].GetChild(0).position;
-            Vector3 p1 = routes[routeNum].GetChild(1).position;
-            Vector3 p2 = routes[routeNum].GetChild(2).position;
-            Vector3 p3 = routes[routeNum].GetChild(3).position;
-
-            while (tParam < 1)
-            {
-                tParam = m_Speed;
-
-                aiPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
-                Debug.Log(aiPosition);
-                transform.position = aiPosition;
-                yield return new WaitForEndOfFrame();
-            }
-
-            tParam = 0f;
-            routeToGo += 1;
-
-            if (routeToGo > routes.Length - 1)
-            {
-                routeToGo = 0;
-            }
-
-            coroutineAllowed = true;
-
-        }
-
     }
 }
